ee88a746ea07ed16df7690bd0ae33f59c7b2aa8c
Samikshaghimire7 <ghimiresamiksha9@gmail.com>
2026-05-06 19:37:47 +0545

    my first commit

---
+++ studentgradesystem.py
@@ -0,0 +1,13 @@
+
+name = input("USER KINDLY INPUT YOUR NAME: ")
+subject = int(input("How many subjects you have? "))
+total = 0
+for i in range(1 , subject+1):
+  marks = float(input("Enter marks for subject" + str(i)+ ":"))
+
+
+total = total + marks
+average = total/subject
+print("STUDENT: " , name)
+print("TOTAL MARKS: ", total)
+print("AVERAGE: " , average)
