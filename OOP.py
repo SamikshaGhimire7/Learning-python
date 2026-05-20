15c010c282bf466d9acf34ad99b59904877bf1a3
Samikshaghimire7 <ghimiresamiksha9@gmail.com>
2026-05-20 12:02:59 +0545

    Learning oop

---
+++ OOP.py
@@ -0,0 +1,12 @@
+class student:
+  pass
+
+
+student1 = student()
+student.name = "Samiksha"
+student.age = 19
+student.course = 'BSC.CSIT'
+
+print(student.name)
+print(student.age)
+print(student.course)
